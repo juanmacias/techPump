f1102ba2f320a915635afb8bfa7206fced6b9d17
Manuel Canga <dev@trasweb.net>
2019-12-28 16:59:50 +0100

    Feat: Add codeception structure for tests and first test

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ cd server && docker-compose up -d
 ## Starting project
 
 Browser [http://sites.techpump.local:8080](http://sites.techpump.local:8080) in your favorite browser
+
+## Running tests
+
+From `app/codeception`( or `/var/www/html/app/codeception` inside of container ) run:
+
+```shell
+php ../vendor/bin/codecept run --steps -d
+```
---
+++ app/codeception/includes/techPump/FunctionalTester.php
@@ -0,0 +1,26 @@
+<?php
+namespace techPump;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause()
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class \techPump\FunctionalTester extends \Codeception\Actor
+{
+    use _generated\\techPump\FunctionalTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+}
