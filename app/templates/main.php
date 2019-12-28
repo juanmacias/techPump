7b845684b8d0335465f8dd5585185680be4002c8
Manuel Canga <dev@trasweb.net>
2019-12-28 21:40:53 +0100

    Feat: Add SitePage, a template for sites, and demo templates

---
+++ app/templates/main.php
@@ -0,0 +1,295 @@
+<div class="listado-chicas">
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-01.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-02.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-03.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-04.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-05.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica chica-grande">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-grande-01.jpg" width="357" height="307" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-06.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-07.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-08.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-09.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-10.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-01.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-02.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-04.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-05.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-06.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-07.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica chica-grande grande-derecha">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-grande-02.jpg" width="357" height="307" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-07.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-08.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-09.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-10.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-01.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-02.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-03.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-04.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-05.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-06.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-07.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica chica-grande">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-grande-03.jpg" width="357" height="307" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-08.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-09.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-10.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-01.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-02.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="chica">
+      <a class="link" href="#" title="">
+         <span class="thumb"><img src="images/thumb-03.jpg" width="175" height="150" alt="" title=""/></span>
+         <span class="nombre-chica"> <span class="ico-online"></span> Nombre de la chica</span>
+         <span id="favorito" class="ico-favorito"></span>
+      </a>
+   </div>
+
+   <div class="clear"></div>
+
+   <a class="btn-mas-modelos" href="#" title="Mostrar más modelos">Siguiente Página</a>
+
+</div>
---
+++ app/templates/top.php
@@ -0,0 +1,16 @@
+<div class="box-header">
+   <div class="header">
+      <h1 class="logo-sitio"><a href="#" title="Cerdas.com">Cerdas.com</a></h1>
+      <div class="tit-webcams">Webcams</div>
+
+      <div class="logo-cum"><a href="#" title="Cumlouder.com">Cumlouder.com</a></div>
+
+      <div class="menu">
+         <a href="#" title="Acceso a las Chicas en Directo">Acceso a las Chicas en Directo</a> <span>|</span>
+         <a href="#" title="Acceso Miembros">Acceso Miembros</a> <span>|</span>
+         <a href="#" title="Compra Créditos">Compra Créditos</a>
+      </div>
+
+      <div class="clear"></div>
+   </div>
+</div>
