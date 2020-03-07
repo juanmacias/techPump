5bf9efb90828b30c3ffeea5ff1225237bd513916
Juan Macias <jmacias@qashops.com>
2020-03-07 12:24:21 +0100

    Update Home.php
    Propuesta de refactoring

--- src/Apps/Sites/Controllers/Home.php
+++ src/Apps/Sites/Controllers/Home.php
@@ -10,75 +10,92 @@ use techPump\Framework\Pages\AppPage;
 use techPump\Framework\Pages\Page;
 use techPump\Framework\Pages\TemplateCache;
 
-class Home extends Controller {
+class Home extends Controller
+{
 
-	final public function getPage(): Page {
-		$page = $this->request->getCurrentNumPage();
+    public const CACHED_MAIN_PAGE = '/cached_main/page_';
+    public const PHP_EXTENSTION = '.php';
+    public const BASE_MAIN_PHP = '/base/main.php';
 
-		$cached_template_file = $this->templates_path . '/cached_main/page_' . $page . '.php';
+    
+    final public function getPage(): Page
+    {
+        $page = $this->request->getCurrentNumPage();
 
-		$this->checkCaches( $cached_template_file);
+        $cached_template_file = $this->getCachedFile($page);
 
-		$home_page = new AppPage( $this->templates_path );
-		$home_page->setMainPage( $cached_template_file );
-		$home_page->addVar( 'site', $this->site );
-		$home_page->addVar( 'page', $page );
+        $this->checkCaches($cached_template_file);
 
-		return $home_page;
-	}
+        $home_page = new AppPage($this->templates_path);
+        $home_page->setMainPage($cached_template_file);
+        $home_page->addVar('site', $this->site);
+        $home_page->addVar('page', $page);
 
-	/**
-	 * Helper: Check if any cache related to cache_template_file( or current page ) can be used .
-	 *
-	 * @param string $cached_template_file
-	 */
-	private function checkCaches(string $cached_template_file) {
-		$cache = AppCache::fromCacheSystem();
+        return $home_page;
+    }
 
-		if ( $cache->isTokenExpired() ) {
-			$cache->updateToken();
-		}
+    /**
+     * Helper: Check if any cache related to cache_template_file( or current page ) can be used .
+     *
+     * @param string $cached_template_file
+     */
+    private function checkCaches(string $cached_template_file):void
+    {
+        $cache = AppCache::fromCacheSystem();
 
-		$this->checkIfHttpCacheCanBeUsed( $cache->getHttpCache( $this->request ) );
+        if ($cache->isTokenExpired()) {
+            $cache->updateToken();
+        }
 
-		$this->checkIfTemplateCacheNeedBeGenerated($cache->getTemplateCache( $cached_template_file ) );
+        $this->checkIfHttpCacheCanBeUsed($cache->getHttpCache($this->request));
 
-		return ;
-	}
+        $this->checkIfTemplateCacheNeedBeGenerated($cache->getTemplateCache($cached_template_file));
+    }
 
-	/**
-	 * Helper: Check HTTP Cache.
-	 *
-	 * This cache is only for current site.
-	 *
-	 * @param HttpCache $cache_http
-	 */
-	private function checkIfHttpCacheCanBeUsed( HttpCache $cache_http ) {
-		if ( !$cache_http->isTokenExpired() ) {
-			$cache_http->useCache();
-			die();
-		}
+    /**
+     * Helper: Check HTTP Cache.
+     *
+     * This cache is only for current site.
+     *
+     * @param HttpCache $cache_http
+     */
+    private function checkIfHttpCacheCanBeUsed(HttpCache $cache_http):void
+    {
+        if (!$cache_http->isTokenExpired()) {
+            $cache_http->useCache();
+            die();
+        }
 
-		$cache_http->sendNewToken();
-	}
+        $cache_http->sendNewToken();
+    }
 
-	/**
-	 * Check if a template cached can be generated.
-	 *
-	 * This cache is only for all site.
-	 *
-	 * @param TemplateCache $cached_template
-	 */
-	private function checkIfTemplateCacheNeedBeGenerated( TemplateCache $cached_template ) {
-		if ( !$cached_template->isCacheExpired() ) {
-			return ;
-		}
+    /**
+     * Check if a template cached can be generated.
+     *
+     * This cache is only for all site.
+     *
+     * @param TemplateCache $cached_template
+     */
+    private function checkIfTemplateCacheNeedBeGenerated(TemplateCache $cached_template):void
+    {
+        if (!$cached_template->isCacheExpired()) {
+            return;
+        }
 
-		$chicas_store = new ChicasRepository( $this->request->getCurrentNumPage() );
+        $chicas_store = new ChicasRepository($this->request->getCurrentNumPage());
 
-		$chicas_list  = $chicas_store->getAll();
+        $chicas_list = $chicas_store->getAll();
 
-		$cached_template->generate( $this->templates_path . '/base/main.php', [ 'chicas_list' => $chicas_list ] );
-	}
+        $cached_template->generate($this->templates_path . self::BASE_MAIN_PHP, ['chicas_list' => $chicas_list]);
+    }
+
+    /**
+     * @param int $page
+     * @return string
+     */
+    private function getCachedFile(int $page): string
+    {
+        return $this->templates_path . self::CACHED_MAIN_PAGE . $page . self::PHP_EXTENSTION;
+    }
 
 }
